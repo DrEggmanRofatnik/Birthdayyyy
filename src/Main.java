688df04bc66b1bf55f9bfa03a9be7ab3cdbad92e
Clarence Martin <marticla000@warren.k12.in.us>
2024-09-26 13:46:23 -0400

    git commit -am "giggity

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Main {
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import.java.time.LocalDateTime;
+public class Main {
+
+    public static void main(String[] args) {
+
+        LocalDateTime birthday = LocalDateTime.of(2006, 11, 19);
+        ZoneId us = ZoneId.of("America/New_York");
+        ZonedDateTime birthdayZoned = ZonedDateTime.of(birthday, us);
+
+        ZoneId london = ZoneId.of("Europe/London");
+        ZonedDateTime birthdayGermany = birthdayZoned.withZoneSameInstant(london);
+        System.out.println(birthdayZoned);
+        System.out.print(birthdayGermany);
+    }
+}
+    }
 }
